5fff316e108ba1228a11ba19d801c0c78acb6510
Dominique McLean <dommclean13@gmail.com>
2019-10-13 20:39:18 -0500

    updated css

--- app.js
+++ app.js
@@ -38,79 +38,20 @@ $("button").on("click", function() {
     });
 });
 
-$("#images").on("click", ".gif", function(event){
-  event.preventDefault();
-  
-  var freeze = $(this).attr("data-animal");
-      if (freeze === "still") {
-  $(this).attr("src", $(this).attr("data-animate"));
-  $(this).attr("data-state", "animate");
-  } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-  }
-});
 
 
 
-// $("#images").on("click, ".gif", function(){
+// $("#images").on("click", ".gif", function(event){
 //   event.preventDefault();
-
-
-//   var state = $(this).attr("data-state");
-
-//     if()
-
-// }
-
-
-
-
-
-
-
-    // my code below
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     })
-
-//     .then(function(response) {
-
-//       // Storing an array of results in the results variable
-
-//       var results = response.data;
-      
-
-//       // ------------------ //
-
-      
-//           // Only taking action if the photo has an appropriate rating
-//           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-
-//             // Creating a div for the gif
-//             var gifDiv = $("<div>");
-
-//             // Storing the result item's rating
-//             var rating = results[i].rating;
-
-//             // Creating a paragraph tag with the result item's rating
-//             var p = $("<p>").text("Rating: " + rating);
-
-//             // Creating an image tag
-//             var personImage = $("<img>");
-
-//             // Giving the image tag an src attribute of a proprty pulled off the
-//             // result item
-//             personImage.attr("src", results[i].images.fixed_height.url);
-
-//             // Appending the paragraph and personImage we created to the "gifDiv" div we created
-//             gifDiv.append(p);
-//             gifDiv.append(personImage);
-
-//             // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-//             $("#images").prepend(gifDiv);
-//           }
-//         }
-//       });
+  
+//   var freeze = $(this).attr("data-animal");
+//       if (freeze === "still") {
+//   $(this).attr("src", $(this).attr("data-animate"));
+//   $(this).attr("data-person", "animate");
+//   } else {
+//       $(this).attr("src", $(this).attr("data-still"));
+//       $(this).attr("data-person", "still");
+//   }
 // });
+
+
